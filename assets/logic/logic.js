dca7fc385adbf4ae0bc3948a263e8a331f7eba03
Matthew Bell <matthewbell1030@gmail.com>
2020-03-25 09:58:42 -0700

    For 'logic.js', I added removed copyPW() function, and cleaned up file.  For 'index.html', I added a an onClick function to the copy button, and a script section to handle the

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -7,24 +7,15 @@ $(document).ready(function() {
   // GLOBAL VARIABLES
     // Field
     const pwField = $("#passwordDisplay");
-    
 
-    
     // Buttons
     const generateBtn = $("#generate");
-    const copyBtn = $("#copy");
 
   // PROCESSES
     // When user clicks '#generate' button...
     $(generateBtn).click(function(event) {
       event.preventDefault();
       pwField.text(generatePW);
-    });
-    // When user clicks '#copy' button...
-    $(copyBtn).click(function(event) {
-      event.preventDefault();
-      const pwContent = pwField.text();
-      copyPW(pwContent);
     });
   // End of document.ready process
 });
@@ -44,22 +35,4 @@ $(document).ready(function() {
     }
     console.log("New Password: ", newPassword);
     return newPassword;
-  };
-
-  // Copy generated password to clipboard 
-  const copyPW = pwContent => {
-    // Check if pwField is blank
-    if (pwContent !== "") {
-      console.log("pwcontent: ", pwContent);
-      // Select contents of pwField
-      $(pwContent).select();
-      // // Set selection range -- for mobile devices
-      // $(pwContent).setSelectionRange(0, 99999);
-      // Copy selection
-      document.execCommand("copy");
-      // Alert user that new password has successfully been copied
-      alert("New password has been copied to clipboard!")
-      } else {
-    alert("No password to copy! \nGenerate a password first.");
-      }
   };
